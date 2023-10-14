044b7f7050bc51b0aaefa18f7f112304749d71ea
Shawna-lee1 <104038577+Shawna-lee1@users.noreply.github.com>
2023-10-14 07:58:42 -0400

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,24 +1,71 @@
-import sys
-import socket
-import os
-import select
-import time
-import signal
+import socket #Enables network operations like creating a server
+import sys #Provides access to some variables used by the interpreter like command-line arguments
+import signal #Allows you to handle asynchronous events or signals.
+import time #provide various time-related functions.
+import threading 
 
-# Define a flag to track whether a signal was received
-shutdown_signal_received = False  # Correct variable name
 
-# Signal handler function to handle SIGQUIT, SIGTERM, and SIGINT
-def signal_handler(sig, frame):
-    global shutdown_signal_received
 
-    if sig in [signal.SIGQUIT, signal.SIGTERM, signal.SIGINT]:
-        print(f"Received signal {sig}. Gracefully shutting down...")
-        shutdown_signal_received = True
+
+### Step 1: Command Line Processing and Signal Handling
+
+not_stopped = True #This global variable acts as a flag to control the main server loop.
+
+#This function is designed to handle signals by changing not_stopped to False and printing a message. 
+#It will be triggered by signal events like keyboard interrupts (Ctrl+C).
+def signal_handler(sig, frame): 
+    global not_stopped
+    not_stopped = False
+    print(f"\nServer shutting down... Signal: {sig}, Frame: {frame}")
+
+#This function continuously reads data from a client socket until it encounters a carriage return and newline ("\r\n").
+#If socket reading times out, it handles the exception and closes the socket.
+def receive_commands(client_socket):
+    b = b""
+
+    while True:
+        # Breaks loop once complete data is received
+        if b.endswith(b"\r\n"):
+            break
+        try:
+            chunk = client_socket.recv(1024)
+        except socket.timeout:
+            sys.stderr.write("ERROR: Timeout waiting for commands from the server.\n")
+            client_socket.close()
+            sys.exit(1)
+
+        b = b + chunk
+
+#receives data from the client socket and accumulates the total number of bytes received.
+def handle_client_connection(client_socket):
+    total_bytes_received = 0
+    while True:
+        data = client_socket.recv(1024) # Buffer size 1024 bytes
+        if not data:
+            break
+        total_bytes_received += len(data)
+
+    print(f"Total bytes received: {total_bytes_received}")
+
+
+### Step 2: Server Socket Initialization and Connection Accepting
+
+#This function creates, binds, and returns a server socket listening on all available interfaces (0.0.0.0) and a specified port.
+def create_server_socket(port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', port))
+    server_socket.listen(10)
+    return server_socket
 
 def main():
+    # Signal handling
+    signal.signal(signal.SIGQUIT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Command-line argument processing
     if len(sys.argv) != 2:
-        sys.stderr.write("ERROR: Invalid number of arguments\n")
+        sys.stderr.write("ERROR: Invalid arguments\n")
         sys.exit(1)
 
     try:
@@ -30,75 +77,52 @@ def main():
         sys.stderr.write("ERROR: Invalid port number\n")
         sys.exit(1)
 
-    # Register signal handlers
-    signal.signal(signal.SIGQUIT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow address reuse
     try:
-        server_socket.bind(("0.0.0.0", port))
-    except OSError as e:
-        if e.errno == 98:
-            sys.stderr.write("ERROR: Address already in use\n")
-            sys.exit(1)
-
-    server_socket.listen(10)
-
-    print("Server is running. To gracefully shut down, press Ctrl+C or send a termination signal...")
-
-    while not shutdown_signal_received:  # Correct variable name
-        client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address}")
-
-        # Send "accio\r\n" command
-        client_socket.send(b"accio\r\n")
-
-        received_data = b""  # Initialize an empty bytes object to store the received data
-        total_received_bytes = 0
-
+        server_socket = create_server_socket(PORT)
+        print(f"Server listening on port {PORT}...")
+    except Exception as e:
+        sys.stderr.write(f"ERROR: {str(e)}\n")
+        sys.exit(1)
+    
+    while not_stopped:
         try:
-            client_socket.setblocking(0)  # Set the socket to non-blocking mode
-            start_time = time.time()
+            server_socket.settimeout(1)
+            #print("Before accept\n")
+            client_socket, client_address = server_socket.accept()
+            #print("After accept\n")
+            client_socket.settimeout(10)
+            
+            print(f"Connection from {client_address}...")
+            
+            client_socket.send(b"accio\r\n")
 
-            while not shutdown_signal_received:  # Correct variable name
-                ready, _, _ = select.select([client_socket], [], [], 10)
+            receive_commands(client_socket)
 
-                if not ready:
-                    # No data received for over 10 seconds
-                    print("Timeout: No data received. Aborting connection.")
-                    client_socket.send(b"ERROR\r\n")
-                    break
+            client_socket.send(b"confirm-accio-again\r\n\r\n")
 
-                data_chunk = client_socket.recv(4096)
-                if not data_chunk:
-                    break
+            receive_commands(client_socket)
+            
+            handle_client_connection(client_socket)
+            
+            client_socket.close()
+            print("Connection closed.")
+            
+        except socket.timeout:
+            #sys.stderr.write("ERROR: No data received for 10 seconds.\n")
+            sys.stderr.write(".")
+        except Exception as e:
+            sys.stderr.write(f"ERROR: {str(e)}\n")
 
-                received_data += data_chunk
-                total_received_bytes += len(data_chunk)
-
-                while b'\r' in received_data or b'\n' in received_data:
-                    if b'\r' in received_data:
-                        line, received_data = received_data.split(b'\r', 1)
-                    else:
-                        line, received_data = received_data.split(b'\n', 1)
-                    line_text = line.decode('utf-8')
-                    line_text = line_text.strip()
-                    if line_text:
-                        print(f"Received {len(line)} bytes")
-                        print(f"Received data: {line_text}")
-
-            print(f"Total received bytes: {total_received_bytes}")
-
-        except KeyboardInterrupt:
-            print("Received Ctrl+C. Gracefully shutting down...")
+### Step 4: Handle Client Connection and Data Receiving
+def handle_client_connection(client_socket):
+    total_bytes_received = 0
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
             break
+        total_bytes_received += len(data)
 
-        client_socket.close()
-
-    server_socket.close()
+    print(f"Total bytes received: {total_bytes_received}")
 
 if __name__ == "__main__":
     main()
- 
